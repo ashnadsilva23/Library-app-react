59c85b209ce3ee346d60e6aa57887eb144cfd3bb
ashnadsilva23 <ashnadsilva2309@gmail.com>
2024-06-11 23:25:14 +0530

    project completed

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const AddBook = () => {
+    
+
+    const[data,changeData]=useState(
+        {
+
+            "title":"",
+            "category":"",
+            "publishedYear":"",
+            "author":"",
+            "description":"",
+            "distributer":"",
+            "publisher":"",
+            "price":""
+            
+        }
+
+    )
+const inputHandler=(event)=>
+    {
+        console.log(data)
+        changeData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>
+        {
+            console.log(data)
+        }
   return (
     <div>
         <Navbar/>
@@ -12,48 +38,45 @@ const AddBook = () => {
                     <div className="row g-3">
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Book Title</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='title' value={data.title} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Category</label>
-                            <select name="" id="" className="form-control">
-                                <option value="">Fiction</option>
-                                <option value="">Novel</option>
-                                <option value="">Drama</option>
-                                <option value="">Horror</option>
+                            <select  id="" className="form-control" name='category' value={data.category} onChange={inputHandler}>
+                                <option value="Fiction">Fiction</option>
+                                <option value="Novel">Novel</option>
+                                <option value="Drama">Drama</option>
+                                <option value="Horror">Horror</option>
                             </select>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label"> Published Year</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='publishedYear' value={data.publishedYear} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Author Name</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='author' value={data.author} onChange={inputHandler} />
 
                         </div>
-                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Description</label>
-                            <textarea name="" id="" className="form-control"></textarea>
+                            <textarea  id="" className="form-control" name='description' value={data.description} onChange={inputHandler}></textarea>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Distributor Name</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='distributer' value={data.distributer} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Publisher Name</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='publisher' value={data.publisher} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Price</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='price' value={data.price} onChange={inputHandler} />
                         </div>
+                       
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label">Edition</label>
-                            <input type="text" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <button className="btn btn-success">Add Book</button>
+                            <button className="btn btn-success" onClick={readValue}>Add Book</button>
 
                         </div>
                        
--- src/components/ViewBooks.jsx
+++ src/components/ViewBooks.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const ViewBooks = () => {
+    const[data,changeData]=useState(
+        [{
+
+            "title":"abcd",
+            "category":"english",
+            "publishedYear":"1890",
+            "author":"apj",
+            "description":"books",
+            "distributer":"Dc Books",
+            "publisher":"xyz",
+            "price":"1200"
+            
+        }]
+
+    )
     return (
         <div>
             <Navbar/>
@@ -16,76 +31,38 @@ const ViewBooks = () => {
                                     <tr>
                                         <th scope="col">sl no</th>
                                         <th scope="col">Book Title</th>
-                                        <th scope="col">Author</th>
+                                        <th scope="col">Category</th>
                                         <th scope="col">Published Year</th>
+                                        <th scope="col">Author</th>
+                                        <th scope="col">Description</th>
+                                        <th scope="col">Distributer Name</th>
+                                        <th scope="col">Publisher </th>
                                         <th scope="col"> price</th>
 
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td>Harry Potter</td>
-                                        <td>John </td>
-                                        <td>1980</td>
-                                        <td>300</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">2</th>
-                                        <td>Jane Eyre</td>
-                                        <td>charlo Bontee qouilo</td>
-                                        <td>1988</td>
-                                        <td>250</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">3</th>
-                                        <td>In Cold Blood</td>
-                                        <td> Trumen Captte</td>
-                                        <td>1917</td>
-                                        <td>150</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">4</th>
-                                        <td>Vanity Fair</td>
-                                        <td> william</td>
-                                        <td>1907</td>
-                                        <td>400</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">5</th>
-                                        <td>Drakula</td>
-                                        <td>Bram Stoker</td>
-                                        <td>1976</td>
-                                        <td>320</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">6</th>
-                                        <td>Lolita</td>
-                                        <td> Wladimir</td>
-                                        <td>1967</td>
-                                        <td>320</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">7</th>
-                                        <td>To kill a monging bird</td>
-                                        <td> Harpe</td>
-                                        <td>1960</td>
-                                        <td>200</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">8</th>
-                                        <td>Alchemist</td>
-                                        <td>Paulo qouilo</td>
-                                        <td>2007</td>
-                                        <td>200</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">9</th>
-                                        <td>Currious incident</td>
-                                        <td> Haddon</td>
-                                        <td>2007</td>
-                                        <td>200</td>
-                                    </tr>
+                                   
+                                  
+                                   
+                               {data.map(
+                                (value,index)=>{
+                                    return <tr>
+                                    <td> {index+1}</td>
+                                    <td> {value.title}</td>
+                                    <td> {value.category}</td>
+                                    <td> {value.publishedYear}</td>
+                                    <td> {value.author}</td>
+                                    <td> {value.description}</td>
+                                    <td> {value.distributer}</td>
+                                    <td> {value.publisher}</td>
+                                    <td> {value.price}</td>
+                                   
+                                </tr>
+                                }
+                               )}
+                                  
+                                    
                                 </tbody>
                             </table>
                     </div>
